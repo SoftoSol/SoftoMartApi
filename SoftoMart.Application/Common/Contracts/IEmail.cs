c16fa60374cd859db8796456e0dd34df63b8ee67
Arham Khan <arhamanees10@gmail.com>
2021-08-09 23:43:25 +0500

    IEmail added

---
+++ SoftoMart.Application/Common/Contracts/IEmail.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftoMart.Application.Common.Contracts
+{
+    public interface IEmail
+    {
+        bool Send(string subject, string bod, string toAddress, string fromAddress);
+        bool Send(string subject, string bod, string toAddress, string fromAddress, string fromName);
+    }
+}
